aee212ca2685807d4d961b9fa389c930b6eac155
unknown <murilocola@gmail.com>
2021-09-20 21:58:20 -0300

    ajuste dag 1

--- dags/my_dag.py
+++ dags/my_dag.py
@@ -1,12 +1,14 @@
 from airflow import DAG
 from airflow.operators.python_operator import PythonOperator
+from airflow.operators.bash import BashOperator
 
 from datetime import datetime, timedelta
 
 def _checking_data():
     print('teste task01')
 
-
+def _checking_teste():
+    print('teste 2')
 
 
 with DAG(
@@ -14,13 +16,25 @@ with DAG(
                 description="DAG é minha",
                 start_date=datetime(2021,9,19),
                 schedule_interval='@daily',
-                max_active_runs=1,
+                #max_active_runs=1,
                 tags=["mcola_my_dag"],
-                catchup=True
+                catchup=False
  ) as dag:
 
     checking_data = PythonOperator(
         task_id="task_1",
         python_callable=_checking_data
         
+    )
+
+    checking_teste = PythonOperator(
+        task_id="task_2",
+        python_callable=_checking_teste
     )    
+
+    checking_bash = BashOperator(
+        task_id="bash_task",
+        bash_command='exit 0'
+    )
+
+    [checking_data,checking_bash]>>checking_teste
